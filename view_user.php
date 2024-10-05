c617c3c5f29995c19a6900e4e75fd1c134d55853
Daniel-Obuya <daniel.nyabuto@strathmore.edu>
2024-10-05 21:02:56 +0300

    Displaying the details

--- view_user.php
+++ view_user.php
@@ -22,29 +22,9 @@ if ($user) {
 </head>
 <body>
 
-<h2>User Details</h2>
+<h2>Registered Users</h2>
 <table border="1">
-    <tr>
-        <th>Full Name</th>
-        <td><?php echo htmlspecialchars($user->getFullName()); ?></td>
-    </tr>
-    <tr>
-        <th>Email</th>
-        <td><?php echo htmlspecialchars($user->getEmail()); ?></td>
-    </tr>
-    <tr>
-        <th>Username</th>
-        <td><?php echo htmlspecialchars($user->getUsername()); ?></td>
-    </tr>
-    <tr>
-        <th>Gender</th>
-        <td><?php echo htmlspecialchars($user->getGender()); ?></td>
-    </tr>
-    <tr>
-        <th>Role</th>
-        <td><?php echo htmlspecialchars($user->getRole()); ?></td>
-    </tr>
-</table>
+  
 
 </body>
 </html>
